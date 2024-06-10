d9eb4bd620866df5bc285348e11253c45a7db40a
shuang he <shuanghe@shuangs-MacBook-Air.local>
2024-06-10 15:27:23 +1200

    Add Specflow Folders

---
+++ Feature/TestPlus.feature
@@ -0,0 +1,5 @@
+Feature: TestSpecflow
+
+@Smoketests
+Scenario: Test Plus
+Test Plus Two Numbers
---
+++ Test/PlusTwoNumber.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using System;
+
+[Binding]
+public class Plus
+{
+     [Given("Test Plus Two Numbers")]
+
+public void PlusTwoNumber()
+{
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    Console.WriteLine("c = " + c);
+}
+}
