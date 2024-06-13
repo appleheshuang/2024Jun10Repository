bfd0d6b41a64f0ab295a9acac3199e04a5e5f439
shuang he <shuanghe@shuangs-MacBook-Air.local>
2024-06-13 14:57:21 +1200

    add test log to test report

--- StepDefinition/CSharpTest.cs
+++ StepDefinition/CSharpTest.cs
@@ -1,32 +1,11 @@
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 using System;
-using Serilog;
 
 [Binding]
 [TestFixture]
 public class Plus
 {
-private TestFixture _testFixture;
-
-    [OneTimeSetUp]
-    public void OneTimeSetUp()
-    {
-        _testFixture = new TestFixture();
-    }
-
-    [OneTimeTearDown]
-    public void OneTimeTearDown()
-    {
-        _testFixture.Dispose();
-    }
-
-    [Test]
-    public void ExampleTest()
-    {
-        Log.Information("This is a test log message.");
-        Assert.Pass();
-    }
 [Test]
 [Given("Test Plus Two Numbers")]
 public void PlusTwoNumber()
@@ -35,7 +14,6 @@ public void PlusTwoNumber()
     int b = 20;
     int c = a + b;
     Console.WriteLine("cNew = " + c);
-    Log.Information("This is a test log message.");
     Assert.Pass();
 }
 }
